d6e138495255b1a6bf7b61844eac486a808369d1
lydia-l3 <122927604+lydia-l3@users.noreply.github.com>
2024-04-09 18:21:33 +0000

    final commit

--- dice.rb
+++ dice.rb
@@ -24,7 +24,7 @@ end
 
 get("/dice/2/6") do
   first_die = rand(1..6)
-        second_die = rand(1..6)
+  second_die = rand(1..6)
   sum = first_die + second_die
 	
   @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
@@ -34,22 +34,20 @@ end
 
 get("/dice/2/10") do
   first_die = rand(1..10)
-        second_die = rand(1..10)
+  second_die = rand(1..10)
   sum = first_die + second_die
 
-        outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 
-        "<h1>2d10</h1>
-   <p>#{outcome}</p>"
+  erb(:two_ten)
 end
 
 get("/dice/1/20") do
   die = rand(1..20)
 
-	outcome = "You rolled a #{die}."
+	@outcome = "You rolled a #{die}."
 
-	"<h1>1d20</h1>
-   <p>#{outcome}</p>"
+  erb(:one_twenty)
 end
 
 get("/dice/5/4") do
@@ -59,8 +57,7 @@ get("/dice/5/4") do
   fourth_die = rand(1..4)
   sum = first_die + second_die + third_die + fourth_die
 
-	outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
+	@outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
 
-	"<h1>5d4</h1>
-   <p>#{outcome}</p>"
+  erb(:five_four)
 end
